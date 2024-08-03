01516d265edc938e610fec3dd3587f3882b4d11d
fhaniin <fatihahhanin@gmail.com>
2024-08-03 10:42:14 +0700

    membuat website lengkap UAS Web Desain

--- script.js
+++
@@ -1,6 +0,0 @@
-// Toggle class active
-const navbarNav = document.querySelector('.navbar-nav');
-// ketika hamburger menu di klik
-document.querySelesctor('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
-};
